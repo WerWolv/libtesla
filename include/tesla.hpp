e6476125258ba078d91943a57aec9dea109a619b
WerWolv <werwolv98@gmail.com>
2020-01-19 18:26:49 +0100

    Added a main include

---
+++ include/tesla.hpp
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2020 WerWolv
+ * 
+ * This file is part of libtesla.
+ * 
+ * libtesla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * libtesla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libtesla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+
+#include "overlay/gui/gui.hpp"
+
+#include "overlay/elements/element.hpp"
+#include "overlay/elements/frame.hpp"
+#include "overlay/elements/list.hpp"
+#include "overlay/elements/list_item.hpp"
+#include "overlay/elements/toggle_list_item.hpp"
+#include "overlay/elements/custom_drawer.hpp"
