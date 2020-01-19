4eced60d451d18de9936f85e8e4f8a4e8c85583c
WerWolv <werwolv98@gmail.com>
2020-01-19 18:26:36 +0100

    EdiZon -> libtesla

--- include/overlay/gui/gui.hpp
+++ include/overlay/gui/gui.hpp
@@ -2,20 +2,20 @@
  * Copyright (C) 2020 natinusala
  * Copyright (C) 2019 WerWolv
  * 
- * This file is part of EdiZon.
+ * This file is part of libtesla.
  * 
- * EdiZon is free software: you can redistribute it and/or modify
+ * libtesla is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  * 
- * EdiZon is distributed in the hope that it will be useful,
+ * libtesla is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with EdiZon.  If not, see <http://www.gnu.org/licenses/>.
+ * along with libtesla.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
