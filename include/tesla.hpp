1b4bc78fc6b16635d1252b06c0ef9b72691a44dc
Adrien BLANCHET <adrien.blanchet@live.fr>
2020-06-06 22:30:59 +0200

    Adding m_footer_title member to the OverlayFrame class. This is allowing the user to change the buttons legend at the bottom of the overlay.

--- include/tesla.hpp
+++ include/tesla.hpp
@@ -1511,7 +1511,9 @@ namespace tsl {
              * @param title Name of the Overlay drawn bolt at the top
              * @param subtitle Subtitle drawn bellow the title e.g version number
              */
-            OverlayFrame(const std::string& title, const std::string& subtitle) : Element(), m_title(title), m_subtitle(subtitle) {}
+            OverlayFrame(const std::string& title, const std::string& subtitle) : Element(), m_title(title), m_subtitle(subtitle) {
+              m_footer_title = "\uE0E1  Back     \uE0E0  OK";
+            }
             virtual ~OverlayFrame() {
                 if (this->m_contentElement != nullptr)
                     delete this->m_contentElement;
@@ -1526,7 +1528,7 @@ namespace tsl {
 
                 renderer->drawRect(15, tsl::cfg::FramebufferHeight - 73, tsl::cfg::FramebufferWidth - 30, 1, a(tsl::style::color::ColorText));
 
-                renderer->drawString("\uE0E1  Back     \uE0E0  OK", false, 30, 693, 23, a(tsl::style::color::ColorText));
+                renderer->drawString(this->m_footer_title.c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
 
                 if (this->m_contentElement != nullptr)
                     this->m_contentElement->frame(renderer);
@@ -1593,10 +1595,15 @@ namespace tsl {
                 this->m_subtitle = subtitle;
             }
 
+            virtual void setFooterTitle(const std::string &footer_title) final {
+              this->m_footer_title = footer_title;
+            }
+
         protected:
             Element *m_contentElement = nullptr;
 
             std::string m_title, m_subtitle;
+            std::string m_footer_title;
         };
 
         /**
