def7e5302cc347125a27860b21f6a70f17701ab3
Damien Zhao <zdm65477730@126.com>
2022-03-24 20:44:24 +0800

    min fix
    Signed-off-by: Damien Zhao <zdm65477730@126.com>

--- include/tesla.hpp
+++ include/tesla.hpp
@@ -1618,12 +1618,12 @@ namespace tsl {
 #ifndef BUILD_STATUS_MONITOR_OVERLAY
                 renderer->drawRect(15, tsl::cfg::FramebufferHeight - 73, tsl::cfg::FramebufferWidth - 30, 1, a(tsl::style::color::ColorText));
 
-                renderer->drawString(renderer->getMainFrameButtonText().c_str().c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
+                renderer->drawString(renderer->getMainFrameButtonText().c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
 #else
                 if (FullMode == true)
                     renderer->drawRect(15, tsl::cfg::FramebufferHeight - 73, tsl::cfg::FramebufferWidth - 30, 1, a(tsl::style::color::ColorText));
                 if (TeslaFPS == 60)
-                    renderer->drawString(renderer->getMainFrameButtonText().c_str().c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
+                    renderer->drawString(renderer->getMainFrameButtonText().c_str(), false, 30, 693, 23, a(tsl::style::color::ColorText));
 #endif
 
                 if (this->m_contentElement != nullptr)
