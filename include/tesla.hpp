71d07b6afa36b06a4e956a42469f4ed087d1f830
ELY M <ELY3M@users.noreply.github.com>
2023-02-23 00:31:03 -0600

    16.0.0 support fix (#59)

--- include/tesla.hpp
+++ include/tesla.hpp
@@ -3662,7 +3662,7 @@ extern "C" {
         tsl::hlp::doWithSmSession([]{
             ASSERT_FATAL(fsInitialize());
             ASSERT_FATAL(hidInitialize());                          // Controller inputs and Touch
-            ASSERT_FATAL(plInitialize(PlServiceType_System));       // Font data. Use pl:s to prevent qlaunch/overlaydisp session exhaustion
+            ASSERT_FATAL(plInitialize(PlServiceType_User));         // Font data. Use pl:s to prevent qlaunch/overlaydisp session exhaustion //16.0.0 fix - PlServiceType_System to PlServiceType_User
             ASSERT_FATAL(pmdmntInitialize());                       // PID querying
             ASSERT_FATAL(hidsysInitialize());                       // Focus control
             ASSERT_FATAL(setsysInitialize());                       // Settings querying
